b25f239ec89777852abed54eb4c44c4e174320ed
Ihor Kuzmenko <alokozam@gmail.com>
2019-05-15 23:29:42 +0300

    Fix car display issue

--- src/CarCard/CarCard.js
+++ src/CarCard/CarCard.js
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 
 import './CarCard.css';
 
+const N_A = 'n/a';
+
 class CarCard extends Component {
     render() {
         const {car: {stats}, car} = this.props;
 
         return (
             <Link className='car-item' to={'/car/'+car.id+'/rides/'}>
-                <div>Model/Transmition type : {car.model} / {car.transmissionType}</div>
+                <div>Model/Transmition type : {car.model || car.brand || N_A} / {car.transmissionType}</div>
                 <div>Plate #: {car.regNumber}</div>
                 <div>Avg rides amount/duration: {stats && stats.count.toFixed(2)} / {stats && stats.duration.toFixed(0)} minutes</div>
                 <div>Avg Load: {stats && stats.load.toFixed(2)}%</div>
@@ -19,11 +21,11 @@ class CarCard extends Component {
 }
 
 CarCard.defaultProps = {
-    id: 'n/a',
-    regNumber: 'n/a',
-    brand: 'n/a',
-    model: 'n/a',
-    color: 'n/a',
+    id: N_A,
+    regNumber: N_A,
+    brand: N_A,
+    model: N_A,
+    color: N_A,
     transmissionType: 0,
     stats: {
         load: 0,
